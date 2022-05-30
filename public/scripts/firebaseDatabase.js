fcac4c9d5b9857c02df0c63df7f9130e33f7a4f5
Joshua Burt <joshuaadburt@gmail.com>
2022-05-30 17:59:15 -0300

    Added the ability to choose which list of code breakers to see

--- public/scripts/firebaseDatabase.js
+++ public/scripts/firebaseDatabase.js
@@ -20,34 +20,88 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getDatabase();
-
+let lists = [];
+let mostRecentEncryption = 1;
+let currentEncryptionNum = mostRecentEncryption;
 
 // Get list of names of code breakers
 const dbRef = ref(db);
-get(child(dbRef, `codeBreakers/name/`)).then((snapshot) => {
+get(child(dbRef, `codeBreakers/`)).then((snapshot) => {
     if (snapshot.exists()) {
-        let nameList = [];
-        let colorList = [];
 
-// Get name and colour values from firebase and insert into two arrays
         snapshot.forEach((child) => {
-            nameList.push(child.val().name);
-            colorList.push(child.val().color);
+            lists.push(child.val());
         });
 
-        let string = "";
+        // Default encryption number
+        let propLists = getPropertyListsByEncryption(currentEncryptionNum);
+        displayCodeBreakers(propLists[0], propLists[1]);
 
-        for(let i = 0; i < nameList.length; i++) {
-            string += "<div style='color: " + colorList[i] + "'>" + nameList[i] + "</div>";
-        }
-
-        document.getElementById("breakerList").innerHTML = "<p>" + string + "</p>"
     } else {
         console.log("No data available");
     }
 }).catch((error) => {
     console.error(error);
 });
+
+function getListByEncryption(encryptionNum) {
+    return lists[encryptionNum];
+}
+
+function isArrayIndex(n) {
+    return !isNaN(parseInt(n, 10));
+}
+
+function getPropertyListsByEncryption(encryptionNum) {
+    let nameList = [];
+    let colorList = [];
+    let currentList = getListByEncryption(encryptionNum);
+
+    for (let n in currentList) {
+        if (currentList.hasOwnProperty(n) && isArrayIndex(n)) {
+            nameList.push(currentList[n].name);
+            colorList.push(currentList[n].color);
+        }
+    }
+
+
+    return [colorList, nameList];
+}
+
+
+function displayCodeBreakers(colorList, nameList) {
+    if(nameList.length > 0) {
+        let string = "";
+
+        for (let i = 0; i < nameList.length; i++) {
+            string += "<div style='color: " + colorList[i] + "'>" + nameList[i] + "</div>";
+        }
+
+        document.getElementById("breakerList").innerHTML = "<p>" + string + "</p>"
+    } else {
+        document.getElementById("breakerList").innerHTML = "<p>No Code Breakers yet!</p>"
+    }
+}
+
+function previousCodeBreakers() {
+    if(currentEncryptionNum > 0) {
+        currentEncryptionNum--;
+        let propLists = getPropertyListsByEncryption(currentEncryptionNum);
+        displayCodeBreakers(propLists[0], propLists[1])
+    }
+    document.getElementById("nextCodeBreaker").disabled = false;
+    document.getElementById("prevCodeBreaker").disabled = currentEncryptionNum === 0;
+}
+
+function nextCodeBreakers() {
+    if(currentEncryptionNum < lists.length - 1) {
+        currentEncryptionNum++;
+        let propLists = getPropertyListsByEncryption(currentEncryptionNum);
+        displayCodeBreakers(propLists[0], propLists[1])
+    }
+    document.getElementById("prevCodeBreaker").disabled = false;
+    document.getElementById("nextCodeBreaker").disabled = currentEncryptionNum === lists.length - 1;
+}
 
 
 function writeUserData(name, color) {
@@ -56,10 +110,12 @@ function writeUserData(name, color) {
     let regex = /(<([^>]+)>)/ig
     let result = name.replace(regex, "");
 
-    set(ref(db, 'codeBreakers/name/' + Date.now()), {
+    set(ref(db, 'codeBreakers/' + mostRecentEncryption + "/" + Date.now()), {
         color: color,
         name: result
     });
 }
 
 window.writeUserData = writeUserData;
+window.previousCodeBreakers = previousCodeBreakers;
+window.nextCodeBreakers = nextCodeBreakers;
