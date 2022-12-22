07338e41fb77990c5d5a84045cb3bbcae9a67a52
Joshua Burt <joshuaadburt@gmail.com>
2022-12-22 14:24:57 -0400

    Reformatting

--- public/scripts/firebaseDatabase.js
+++ public/scripts/firebaseDatabase.js
@@ -156,30 +156,25 @@ function writeUserData(name, color) {
     }
 }
 
-function fromConsole()
-{
-    var stack;
-    try
-    {
+function fromConsole() {
+    let stack;
+    try {
         // Throwing the error for Safari's sake, in Chrome and Firefox
         // var stack = new Error().stack; is sufficient.
         throw new Error();
-    }
-    catch (e)
-    {
+    } catch(e) {
         stack = e.stack;
     }
-    if (!stack)
+    if(!stack)
         return false;
 
-    var lines = stack.split("\n");
-    for (var i = 0; i < lines.length; i++)
-    {
-        if (lines[i].indexOf("at Object.InjectedScript.") >= 0)
+    let lines = stack.split("\n");
+    for(let i = 0; i < lines.length; i++) {
+        if(lines[i].indexOf("at Object.InjectedScript.") >= 0)
             return true;   // Chrome console
-        if (lines[i].indexOf("@debugger eval code") == 0)
+        if(lines[i].indexOf("@debugger eval code") === 0)
             return true;   // Firefox console
-        if (lines[i].indexOf("_evaluateOn") == 0)
+        if(lines[i].indexOf("_evaluateOn") === 0)
             return true;   // Safari console
     }
     return false;
